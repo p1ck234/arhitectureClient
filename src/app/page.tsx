d4785b763d40d8b3f197380ebfd427410edde3c8
p1ck23 <dagobbo17@gmail.com>
2024-10-31 14:49:13 +0400

    setting project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,46 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import Cards from "@/components/Cards/Cards";
+import Footer from "@/components/footer/Footer";
+import Header from "@/components/header/Header";
+import Main from "@/components/main/Main";
+import Tabs from "@/components/tabs/Tabs";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import styles from "./page.module.css"
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [selectedTag, setSelectedTag] = useState("Architectural Design");
+  const [allCards, setAllCards] = useState([]);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  useEffect(() => {
+    // Получаем данные из Strapi
+    const fetchCards = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:1337/api/articles?populate=*"
+        );
+        setAllCards(response.data.data);
+      } catch (error) {
+        console.error("Ошибка при получении данных из Strapi", error.response);
+      }
+    };
+
+    fetchCards();
+  }, []);
+
+  const filteredCards = allCards.filter(
+    (card) =>
+      card.Name && card.Name.length > 0 && card.Name[0].Name === selectedTag
+  );
+
+  return (
+    <div className={styles.container}>
+      <Header />
+      <Main />
+      <Tabs onSelectTag={setSelectedTag} />
+      <Cards cards={filteredCards} />
+      <Footer />
     </div>
   );
 }
